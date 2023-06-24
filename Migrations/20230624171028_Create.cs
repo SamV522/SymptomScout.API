afaf5cfe3e4d7dc04aedabcd1a243bda6e1386d2
Sam Vlahos <Sam.V_@hotmail.com>
2023-06-25 05:11:09 +1000

    * rejig db model
    * symptom -> diagnosis is now synchronous many to many

--- Migrations/20230624171028_Create.cs
+++ Migrations/20230624171028_Create.cs
@@ -31,17 +31,39 @@ namespace SymptomScout.API.Migrations
                     SymptomId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DiagnosisId = table.Column<int>(type: "int", nullable: true)
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Symptoms", x => x.SymptomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiagnosisSymptom",
+                columns: table => new
+                {
+                    DiagnosesDiagnosisId = table.Column<int>(type: "int", nullable: false),
+                    SymptomsSymptomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiagnosisSymptom", x => new { x.DiagnosesDiagnosisId, x.SymptomsSymptomId });
                     table.ForeignKey(
-                        name: "FK_Symptoms_Diagnoses_DiagnosisId",
-                        column: x => x.DiagnosisId,
+                        name: "FK_DiagnosisSymptom_Diagnoses_DiagnosesDiagnosisId",
+                        column: x => x.DiagnosesDiagnosisId,
                         principalTable: "Diagnoses",
-                        principalColumn: "DiagnosisId");
+                        principalColumn: "DiagnosisId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiagnosisSymptom_Symptoms_SymptomsSymptomId",
+                        column: x => x.SymptomsSymptomId,
+                        principalTable: "Symptoms",
+                        principalColumn: "SymptomId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.UniqueConstraint(
+                        name: "UC_DiagnosisSymptom_DiagnosisId_SymptomId",
+                        columns: x => new { x.DiagnosesDiagnosisId, x.SymptomsSymptomId }
+                        );
                 });
 
             migrationBuilder.CreateIndex(
@@ -51,9 +73,9 @@ namespace SymptomScout.API.Migrations
                 unique: true);
 
             migrationBuilder.CreateIndex(
-                name: "IX_Symptoms_DiagnosisId",
-                table: "Symptoms",
-                column: "DiagnosisId");
+                name: "IX_DiagnosisSymptom_SymptomsSymptomId",
+                table: "DiagnosisSymptom",
+                column: "SymptomsSymptomId");
 
             migrationBuilder.CreateIndex(
                 name: "UC_Symptom_Name",
@@ -66,10 +88,13 @@ namespace SymptomScout.API.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Symptoms");
+                name: "DiagnosisSymptom");
 
             migrationBuilder.DropTable(
                 name: "Diagnoses");
+
+            migrationBuilder.DropTable(
+                name: "Symptoms");
         }
     }
 }
