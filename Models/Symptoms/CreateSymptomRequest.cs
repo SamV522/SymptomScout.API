fa7ee4eb0e3b1084889b39334e194c6343d6b4b0
Sam Vlahos <Sam.V_@hotmail.com>
2023-06-28 03:30:59 +1000

    * bulk create symptom/diagnosis

--- Models/Symptoms/CreateSymptomRequest.cs
+++ Models/Symptoms/CreateSymptomRequest.cs
@@ -9,14 +9,23 @@ namespace SymptomScout.API.Models.Symptoms
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }
-        [Required]
-        public IList<Diagnosis> Diagnoses { get; set; }
+        public IList<Diagnosis>? Diagnoses { get; set; }
 
         public CreateSymptomRequest(string name, string description, IList<Diagnosis> diagnoses) 
         {
             Name = name;
             Description = description;
             Diagnoses = diagnoses;
+        }
+
+        public static explicit operator Symptom(CreateSymptomRequest createSymptomRequest)
+        {
+            return new Symptom()
+            {
+                Name = createSymptomRequest.Name,
+                Description = createSymptomRequest.Description,
+                Diagnoses = new List<Diagnosis>()
+            };
         }
     }
 }
